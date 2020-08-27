936efac7421895ac800b1d0002af10f8c995499e
InverseTriangle <1528575041@qq.com>
2020-08-27 17:31:30 +0800

    删除了第三方组件的demo

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import {Text, View} from 'react-native';
 import * as Demo1 from './demo1/index';
 import * as Demo2 from './demo2/index';
 import * as Demo3 from './demo3/index';
+import * as Demo4 from './demo4/index';
 
 export default class Root extends Component {
   render() {
@@ -26,7 +27,10 @@ export default class Root extends Component {
           {/*<TextInputDemo/>*/}
           {/*<ScrollViewDemo/>*/}
 
-          <Demo2.AlertDemo/>
+          {/*<Demo2.AlertDemo/>*/}
+
+        {/*<Demo4.A1Swiper/>*/}
+        <Demo4.A2ReactNavigation/>
       </View>
     );
   }
